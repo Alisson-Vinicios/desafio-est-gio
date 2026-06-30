947cf2d9449413fbe10f54b752d5b72ebc3119b7
Alisson Araújo <alissonvinicios2@gmail.com>
2026-06-30 09:36:26 -0300

    Imprementando rotas de API e integrando com endpoints com a fila de transações, feito testes unitários

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,59 @@
 const express = require('express');
+const crypto = require('crypto');
+const db = require('./database');
+const { enquere, enqueue } = require('./queue');
+
 const app = express();
 const PORT = 3000;
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json({
-        message: "Bem-vindo à API do AgilBank! Servidor rodando com sucesso."
+//Rota listar contas
+app.get('/accounts', (req, res) => {
+    return res.json(db.accounts);
+});
+
+//Rota listar historico de transações
+app.get('/transactions', (req, res) => {
+    return res.json(db.transactions);
+});
+
+//Rota criar movimentação
+app.post('/transactions', (req, res) => {
+    const { accountOriginId, accountDestinationId, type, amount } = req.body;
+//Validações básicas
+    if (!accountOriginId || !type || !amount){
+        return res.status(400).json({error: "Campos obrigatórios: accountOriginId, type, amount."});
+    }
+
+    if(amount <= 0){
+        return res.status(400).json({error: "O valor da operação deve ser maior que 0"});
+    }
+
+    if(type !== 'SAQUE' && type !== 'TRANSFERENCIA'){
+        return res.status(400).json({error: "Tipo de operação inválida. Use SAQUE ou TRANSFERENCIA."});
+    }
+
+    const transactionBlock = {
+        transctionId: crypto.randomUUID(),
+        accountOriginId,
+        accountDestinationId: type === 'TRANSFERENCIA' ? accountDestinationId: null,
+        type,
+        amount: parseFloat(amount),
+        fee: 0,
+        timestamp: new Date().toISOString(),
+        status: "PENDING",
+        failureReason: null
+    };
+
+    enqueue(transactionBlock);
+
+    return res.status(202).json({
+        message: "Transação recebida e enviada para processamento.",
+        transaction: transactionBlock
     });
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor ativo e rodando na porta http://localhost:${PORT}`)
-})
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
